4099d5498bb7c0fae9f42d3e9bff1117134bccca
André Sortland <arsortland@gmail.com>
2022-03-17 21:27:49 +0100

    Skeleton files, JS programming next

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1> Restaurant Page</h1>
+
+<p> This project is me learning and using Webpack to build a single-page
+webpage with tabs using only javascript to render the content on screen</p>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import "./home.js";
+import BG from "./bg.jpeg";
+
+// const comp = () => {
+//   const element = document.createElement("div");
+//   element.innerHTML = "testing22";
+//   element.classList.add("hello");
+
+//   return element;
+// };
+
+// document.body.appendChild(comp());
