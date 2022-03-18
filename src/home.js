cb8263a4daa3d0eaaef1899fb437ede8972928ca
André Sortland <arsortland@gmail.com>
2022-03-18 11:51:12 +0100

    Made template and started content on home page, follow comments in code

--- src/home.js
+++ src/home.js
@@ -1,6 +1,24 @@
-export const createel = () => {
-  const text = document.createElement("h1");
-  text.textContent = "TEST";
+export const welcomecont = () => {
+  const welcometext = document.createElement("div");
+  welcometext.classList.add("welcometext");
+  welcometext.setAttribute("style", "white-space: pre;");
+  welcometext.textContent = "Welcome to our Restaurant!\r\n";
+  welcometext.textContent +=
+    "Here at our establishment we pride ourselves with only using the best in-season ingredients\r\n";
+  welcometext.textContent +=
+    "At the same time we have a vision to think sustainability and has been meat-free since 2009!\r\n";
+  welcometext.textContent +=
+    "Come try out our menu, you will not be dissapointed!";
 
-  return text;
+  return welcometext;
+};
+
+export const aboutcont = () => {
+  //about content here:
+};
+
+export const staffcont = () => {
+  //pic of fantasy-staff:
+  const staffpic = document.createElement("img");
+  // find pic: staffpic.src("")
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import "./style.css";
-import { createel } from "./home";
+//import { createel } from "./home";
+import * as homejs from "./home";
 
 const container = document.querySelector(".container");
-
 const header = document.createElement("div");
-header.classList.add("header");
-
+const nav = document.createElement("div");
+const home = document.createElement("button");
+const menu = document.createElement("button");
+const contact = document.createElement("button");
 const content = document.createElement("div");
-content.classList.add("content");
-
 const infobottom = document.createElement("div");
+
+home.textContent = "Home";
+menu.textContent = "Menu";
+contact.textContent = "Contact";
+
+header.classList.add("header");
+nav.classList.add("nav");
+content.classList.add("content");
 infobottom.classList.add("infobottom");
 
 container.appendChild(header);
 container.appendChild(content);
 container.appendChild(infobottom);
+header.appendChild(nav);
+nav.appendChild(home);
+nav.appendChild(menu);
+nav.appendChild(contact);
 
-header.appendChild(createel());
+//instead of this, start with HOME set to active.
+content.appendChild(homejs.welcomecont());
+
+home.addEventListener("click", () => {
+  //load homecontent.
+  content.textContent = "";
+  content.appendChild(homejs.welcomecont());
+});
+
+menu.addEventListener("click", () => {
+  //load menucontent.
+  content.textContent = "";
+});
+
+contact.addEventListener("click", () => {
+  //load contactcontent.
+  content.textContent = "";
+});
 
 //Tenke gjennom hvordan "content" klassen skal organiseres mellom tabs
 //Lage noe animasjon til homepage? ALternativt en loader?
